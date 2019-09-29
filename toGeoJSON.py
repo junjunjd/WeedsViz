f35317a13d93dffab3671f89ce13fa607b5c4450
Junjun Dong <junjundong@junjuns-macbook.lan>
2019-09-28 23:34:56 -0700

    Treatment Count All Time

--- toGeoJSON.py
+++ toGeoJSON.py
@@ -14,20 +14,39 @@ SITE_DICT = 'site_dict.json'
 site_dict = json.load(open(SITE_DICT))
 
 
+def get_sites(sitefile: str = SITES):
+    """Get the site data in a DataFrame.
+
+    :param sitefile: Path to the file with site IDs and acreage
+    :return: A DataFrame with the SITEID and Site_Name for each site
+    :rtype: pandas.DataFrame
+    """
+    df = pd.read_csv(sitefile)
+    return df.loc[:, ['SiteID', 'Site_Name', 'Date']]
+
+
+sites = get_sites()
+
+site_count = sites['Site_Name'].value_counts()
+
+print (site_count)
+""" 
 
 geojson = {
     "type": "FeatureCollection",
     "features": [
-    {
-        "type": "Feature",
-        "geometry" : {
-            "type": "Point",
-            "coordinates": [d, d["lat"]],
+        {
+            "type": "Feature",
+            "properties": {
+                "Treatment": n,
             },
-        "properties" : d,
-     } for d in data]
+            "geometry": {
+                "type": "Point",
+                "coordinates": [site_dict.get(m, "Site Name Not Found")[1],site_dict.get(m, "Site Name Not Found")[0]],
+            },
+        } for m, n in site_count.items()]
 }
 
 
-output = open(out_file, 'w')
-json.dump(geojson, output)
+output = open('treat_count_all.json', 'w')
+json.dump(geojson, output, indent=1) """
